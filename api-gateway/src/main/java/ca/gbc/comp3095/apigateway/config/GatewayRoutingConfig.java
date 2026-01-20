05c2442df8c5c2617cec0eebadb3caf245767258
James Jones <jjsfitnesstoronto@gmail.com>
2026-01-20 13:41:58 -0500

    Continuing debug of api-gateway/keycloak Postman testing

--- api-gateway/src/main/java/ca/gbc/comp3095/apigateway/config/GatewayRoutingConfig.java
+++ api-gateway/src/main/java/ca/gbc/comp3095/apigateway/config/GatewayRoutingConfig.java
@@ -13,24 +13,24 @@ public class GatewayRoutingConfig {
         return builder.routes()
                 // Course Service
                 .route("course-service", r -> r.path("/api/v1/courses/**")
-                        .filters(f -> f.addRequestHeader("Authorization", "#{request.headers.Authorization}"))
                         .uri("http://course-service:8080"))
+
                 // Grade Service
                 .route("grade-service", r -> r.path("/api/v1/grades/**")
-                        .filters(f -> f.addRequestHeader("Authorization", "#{request.headers.Authorization}"))
                         .uri("http://grade-service:8081"))
+
                 // Reminder Service
                 .route("reminder-service", r -> r.path("/api/v1/reminders/**")
-                        .filters(f -> f.addRequestHeader("Authorization", "#{request.headers.Authorization}"))
                         .uri("http://reminder-service:8082"))
+
                 // Task Service
                 .route("task-service", r -> r.path("/api/v1/tasks/**")
-                        .filters(f -> f.addRequestHeader("Authorization", "#{request.headers.Authorization}"))
                         .uri("http://task-service:8083"))
+
                 // User Service
                 .route("user-service", r -> r.path("/api/v1/users/**")
-                        .filters(f -> f.addRequestHeader("Authorization", "#{request.headers.Authorization}"))
                         .uri("http://user-service:8089"))
+
                 .build();
     }
 
